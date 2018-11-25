e4b5898699ed7e25a3006718ded0f2a749d30d71
splatte <sp@x-berg.de>
2018-11-25 14:56:58 +0800

    Use ts-jest to run tests with typescript

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const { defaults: tsjPreset } = require('ts-jest/presets');
+
+module.exports = {
+  ...tsjPreset,
+  preset: 'react-native',
+  transform: {
+    ...tsjPreset.transform,
+    '\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+  },
+  globals: {
+    'ts-jest': {
+      babelConfig: true,
+    }
+  }
+};
